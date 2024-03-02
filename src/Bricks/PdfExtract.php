13e230eddcd8041b8120404a6b2a7879f73a06d6
Dieter Coopman <dieter@deltasolutions.be>
2024-03-02 10:32:24 +0100

    add pdf extract brick
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

---
+++ src/Bricks/PdfExtract.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Structurize\Structurize\Bricks;
+
+use Structurize\Structurize\StructurizeApi;
+
+class PdfExtract extends StructurizeApi implements Brick
+{
+
+    /**
+     * @param string $input
+     * @return void
+     */
+    public function parameters(string $input)
+    {
+        $this->input = $input;
+        return $this;
+    }
+
+    public function as($name)
+    {
+        $this->as = $name;
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return json_encode(["brick" => "pdf.extract", "parameters" => ["input" => $this->input], "as" => $this->as]);
+    }
+}
