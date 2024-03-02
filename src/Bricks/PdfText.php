139cebdc644e4f557ba5278713371fdfa050da4a
Dieter Coopman <dieter@deltasolutions.be>
2024-03-02 10:32:18 +0100

    add pdf text brick
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

---
+++ src/Bricks/PdfText.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Structurize\Structurize\Bricks;
+
+use Structurize\Structurize\StructurizeApi;
+
+class PdfText extends StructurizeApi implements Brick
+{
+
+    /**
+     * @param string $input
+     * @return void
+     */
+    public function parameters(string $input)
+    {
+        $this->input = $input;
+        return $this;
+    }
+
+    public function as($name)
+    {
+        $this->as = $name;
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return json_encode(["brick" => "pdf.text", "parameters" => ["input" => $this->input], "as" => $this->as]);
+    }
+}
