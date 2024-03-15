6a14a9c60c790a0f4f988effbd01d50beeed6b4b
Dieter Coopman <dieter@deltasolutions.be>
2024-03-15 16:09:51 +0100

    add peppol send
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

---
+++ src/Bricks/PeppolSend.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Structurize\Structurize\Bricks;
+
+use Structurize\Structurize\StructurizeApi;
+
+class PeppolSend extends StructurizeApi implements Brick
+{
+
+    private $name;
+    private $ubl;
+
+    public function parameters(string $name, string $ubl)
+    {
+        $this->name = $name;
+        $this->ubl = $ubl;
+        return $this;
+    }
+    
+    public function __toString()
+    {
+        return json_encode(["brick" => "peppol.send", "parameters" => ["name" => $this->name, "ubl" => $this->ubl]]);
+    }
+}
