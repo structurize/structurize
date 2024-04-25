438c7ec3983608a38f5e8b2be03e544043a491ee
Dieter Coopman <dieter@deltasolutions.be>
2024-04-25 16:34:01 +0200

    add download and download url
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/StructurizeApi.php
+++ src/StructurizeApi.php
@@ -65,4 +65,13 @@ class StructurizeApi
             return $this->call('building', ['building' => $building]);
         }
     }
+
+    static function download(string $uuid){
+        return self::call('download/'.$uuid, [], 'GET');
+    }
+
+    static function getDownloadUrl(string $uuid){
+        return $_ENV['STRUCTURIZE_API_URL'] . 'download/'.$uuid;
+    }
+
 }
