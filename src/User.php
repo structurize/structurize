ae3930df568b0ad71afd44e973be04debaed7348
Dieter Coopman <dieter@deltasolutions.be>
2024-09-28 16:12:16 +0200

    refactor user
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- src/User.php
+++ src/User.php
@@ -5,9 +5,22 @@ namespace Structurize\Structurize;
 class User extends StructurizeApi
 {
     private $endpoint = 'me';
+    public $info;
 
-    public function info(){
-        return $this->call($this->endpoint, [], 'GET');
+    public function __construct()
+    {
+        $this->info = $this->call($this->endpoint,[], 'GET');
+        return $this;
+    }
+
+    public function info()
+    {
+        return $this->info;
+    }
+
+    public function __toString()
+    {
+        return json_encode($this->info);
     }
 
 }
