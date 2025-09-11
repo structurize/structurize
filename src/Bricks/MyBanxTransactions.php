1408cfa8a7086d2c80d701866584fd9da17b2eeb
Ashim De Coster <ashim@deltasolutions.be>
2025-09-11 10:44:07 +0200

    add transaction params

--- src/Bricks/MyBanxTransactions.php
+++ src/Bricks/MyBanxTransactions.php
@@ -9,17 +9,24 @@ class MyBanxTransactions extends StructurizeApi implements Brick
 
     private array $identifiers;
     private string $bankAccountId;
+    private int $page_id;
+    private array $transactions;
+    private array $params;
 
-    public function __construct(array $identifiers, string $bankAccountId)
+    public function __construct(array $identifiers, string $bankAccountId, int $page_id = 0, array $transactions = [], array $params = [])
     {
         $this->identifiers = $identifiers;
         $this->bankAccountId = $bankAccountId;
+        $this->page_id = $page_id;
+        $this->transactions = $transactions;
+        $this->params = $params;
+
         return $this;
     }
 
     public function __toString()
     {
 
-        return json_encode(["brick" => "mybanx.transactions", "parameters" => ["identifiers" => $this->identifiers, "bankAccountId" => $this->bankAccountId]]);
+        return json_encode(["brick" => "mybanx.transactions", "parameters" => ["identifiers" => $this->identifiers, "bankAccountId" => $this->bankAccountId, "page_id" => $this->page_id, "transactions" => $this->transactions, "params" => $this->params]]);
     }
 }
