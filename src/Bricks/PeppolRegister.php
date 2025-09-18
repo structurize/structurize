d740e22438dd44d7fb7bc2c86d5c461d7802d92a
Ashim De Coster <ashim@deltasolutions.be>
2025-09-18 16:56:48 +0200

    add company name support

--- src/Bricks/PeppolRegister.php
+++ src/Bricks/PeppolRegister.php
@@ -9,18 +9,19 @@ class PeppolRegister extends StructurizeApi implements Brick
 
     private $identifier;
 
-    public function __construct(string $email, string $firstname, string $lastname, string $identifier)
+    public function __construct(string $email, string $firstname, string $lastname, string $identifier, string $company = null)
     {
-        $this->email = $email;
-        $this->firstname = $firstname;
-        $this->lastname = $lastname;
+        $this->email      = $email;
+        $this->firstname  = $firstname;
+        $this->lastname   = $lastname;
         $this->identifier = $identifier;
+        $this->company    = $company;
         return $this;
     }
 
     public function __toString()
     {
 
-        return json_encode(["brick" => "peppol.register", "parameters" => ["email" => $this->email, "firstname" => $this->firstname, "lastname" => $this->lastname, "identifier" => $this->identifier]]);
+        return json_encode(["brick" => "peppol.register", "parameters" => ["email" => $this->email, "firstname" => $this->firstname, "lastname" => $this->lastname, "identifier" => $this->identifier, "company" => $this->company ?? '-']]);
     }
 }
